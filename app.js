69d424baf4e3f707e269a0e6202ac9104fe6fb17
Emir Karşıyakalı <emirkarsiyakali@gmail.com>
2018-04-06 16:01:01 +0300

    added schedule

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-var dataURL = 'https://phpkonf.org/data.json';
+var dataURL = 'data.json';
 
 var App = new Vue({
-    el: '#speakers',
+    el: '#app',
     data: {
-        speakers: []
+        speakers: [],
+        schedule: []
     },
     mounted() {
-        var self = this;
-
-        $.getJSON(dataURL, function (data) {
-            self.speakers = data;
-        });
+        axios.get(
+           dataURL
+        ).then(
+            response => {
+                this.speakers = response.data;
+            }
+        );
     },
     methods: {
+        sessions: function() {
+            for (i = 0; i < this.speakers; i++) {
+                for (j = 0; j < this.speakers[i].schedule; j++) {
+                    this.schedule.push(this.speakers[i].schedule[j]);
+                }
+            }
+
+            return this.schedule;
+        },
+    },
+    computed: {
         randomList: function (rand) {
             return rand.sort(function () {
                 return 0.5 - Math.random()
